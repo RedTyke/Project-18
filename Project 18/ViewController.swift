3733153653fbc8e57af3d293c48f731b7c4577a3
John McNiffe <john@johns-imac.local>
2017-09-30 20:22:57 +0100

    Initial Commit

---
+++ Project 18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 18
+//
+//  Created by John McNiffe on 30/09/2017.
+//  Copyright © 2017 John McNiffe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
